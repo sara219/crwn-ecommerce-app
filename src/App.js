0f8aef755d1e3cff8783b4d8d8f0be68d71a1bf5
sara219 <sara97abuawwad@gmail.com>
2022-11-24 12:06:38 +0200

    add images to the obj and add to the component

--- src/App.js
+++ src/App.js
@@ -5,30 +5,40 @@ const App = () => {
     {
       id: 1,
       title: 'Hats',
+      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
     },
     {
       id: 2,
       title: 'Jackets',
+      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
     },
     {
       id: 3,
-      title: 'Sneakers',
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
     },
     {
       id: 4,
       title: 'Women',
+      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
     },
     {
       id: 5,
       title: 'Men',
+      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
     },
   ]
 
   return (
     <div className='categories-container'>
-      {categories.map(({ title, id }) => (
+      {categories.map(({ title, id, imageUrl }) => (
         <div key={id} className='category-container'>
-          <div  className="background-image"/>
+          <div
+            className='background-image'
+            style={{
+              backgroundImage: `url(${imageUrl})`,
+            }}
+          />
           <div className='category-body-container'>
             <h2>{title}</h2>
             <p>Shop Now</p>
