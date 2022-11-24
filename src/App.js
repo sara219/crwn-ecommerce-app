6aeefca9de194d4829e20864f9cb54248c7972c5
sara219 <sara97abuawwad@gmail.com>
2022-11-24 11:33:31 +0200

    Scaffolding the project

---
+++ src/App.js
@@ -0,0 +1,48 @@
+const App = () => {
+  return (
+    <div className='categories-container'>
+      {/* Hat Category */}
+      <div className='category-container'>
+        {/* <img/> */}
+        <div className='category-body-container'>
+          <h2>Hats</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* Jackets Category */}
+      <div className='category-container'>
+        {/* <img/> */}
+        <div className='category-body-container'>
+          <h2>Jackets</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* Sneakers Category */}
+      <div className='category-container'>
+        {/* <img/> */}
+        <div className='category-body-container'>
+          <h2>Sneakers</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* Women Category */}
+      <div className='category-container'>
+        {/* <img/> */}
+        <div className='category-body-container'>
+          <h2>Women</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+      {/* Men Category */}
+      <div className='category-container'>
+        {/* <img/> */}
+        <div className='category-body-container'>
+          <h2>Men</h2>
+          <p>Shop Now</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
