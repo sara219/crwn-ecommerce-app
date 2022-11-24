43977c9e6cef41a922ef68520bb9c573f9309a8b
sara219 <sara97abuawwad@gmail.com>
2022-11-24 22:31:57 +0200

    Navigation Bar Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Outlet } from 'react-router-dom'
 import Home from './routes/home/home.component'
+
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>i am navigation</h1>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
 
 const Shop = () => {
   return <h1>Hi there</h1>
@@ -8,7 +19,9 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path='/home' element={<Home />}>
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        {/* index if its tue its match the parent component */}
         <Route path='shop' element={<Shop />} />
       </Route>
     </Routes>
