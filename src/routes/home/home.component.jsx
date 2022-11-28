6ece5d1ac49d5fcb08a0df367ad1b781046ef730
sara219 <sara97abuawwad@gmail.com>
2022-11-28 21:47:48 +0200

    remove import react

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,5 +1,4 @@
 import { Outlet } from 'react-router-dom'
-
 import Directory from '../../components/directory/directory.component'
 
 const Home = () => {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,6 +15,9 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
+          <Link className='nav-link' to='/signIn'>
+            SIGNIN
+          </Link>
         </div>
       </div>
       <Outlet />
