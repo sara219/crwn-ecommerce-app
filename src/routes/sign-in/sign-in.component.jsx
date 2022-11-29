3eef8751d65a7b4499cecdb00a9ec06863037cf4
sara219 <sara97abuawwad@gmail.com>
2022-11-29 19:54:37 +0200

    add useeffect to get the result to sing in page after rendring the page and redirect the singin

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,25 @@
+// useEffect => perform side effects in your components
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
 import {
+  auth,
   signInWithGooglePopup,
   createUserDocFromAuth,
+  signInWithGoogleRedirect,
 } from '../../utils/firebase/firebase.utils'
 
 const SignIn = () => {
+  useEffect(() => {
+    const getResult = async () => {
+      const response = await getRedirectResult(auth)
+      if (response) {
+        const userDocRef = await createUserDocFromAuth(response.user)
+        console.log(userDocRef)
+      }
+    }
+    getResult()
+  }, [])
+
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup()
     const userDocRef = await createUserDocFromAuth(user)
@@ -13,6 +29,9 @@ const SignIn = () => {
     <div>
       <h1>SignIn Page</h1>
       <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+      <button onClick={signInWithGoogleRedirect}>
+        Sign in with Google Redirect
+      </button>
     </div>
   )
 }
