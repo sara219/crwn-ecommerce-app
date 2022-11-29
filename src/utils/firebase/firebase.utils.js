3059edb478f2dee77ed175178686126956ffcecc
sara219 <sara97abuawwad@gmail.com>
2022-11-29 13:50:09 +0200

    add firebase configuration

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,18 @@
+ // Import the functions you need from the SDKs you need
+ import { initializeApp } from 'firebase/app'
+ 
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAM5xt-GSFnImE1ZqXMSBM7VjWaoUEpWh8',
+  authDomain: 'crwn-db0-99f50.firebaseapp.com',
+  projectId: 'crwn-db0-99f50',
+  storageBucket: 'crwn-db0-99f50.appspot.com',
+  messagingSenderId: '536248071393',
+  appId: '1:536248071393:web:b2dcfcd69799d8b523b3a0',
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
