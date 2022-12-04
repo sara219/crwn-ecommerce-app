0659f1f74cf8f169f6da7fef381784ba3262029d
sara219 <sara97abuawwad@gmail.com>
2022-12-04 22:21:48 +0200

    add grid display to the products container

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { ProductsContext } from '../../context/products.context'
 import ProductCard from '../../components/product-card/product-card.component'
+import './shop.styles.scss'
 
 const Shop = () => {
   const { products } = useContext(ProductsContext)
