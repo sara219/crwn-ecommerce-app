78cbc9e0ec9ae550df8681ab57a0b2cbc51c79ce
sara219 <sara97abuawwad@gmail.com>
2022-12-04 19:28:02 +0200

    get data from json file and add the shop route inside routes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Authentication from './routes/authentication/authentication.component'
 import Home from './routes/home/home.component'
 import Navigation from './routes/navigation/navigation.component'
-
-const Shop = () => {
-  return <h1>Hi there</h1>
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
   return (
@@ -13,6 +10,7 @@ const App = () => {
       <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
         {/* index if its tue its match the parent component */}
+        <Route path='shop' element={<Shop />} />
         <Route path='auth' element={<Authentication />} />
       </Route>
     </Routes>
