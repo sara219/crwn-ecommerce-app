f0963a50064dedc1e330253adbf2008e7ad4bed8
sara219 <sara97abuawwad@gmail.com>
2022-12-05 18:47:00 +0200

    create context for add item to cart and create a helper function for that

--- src/context/cart.context.jsx
+++ src/context/cart.context.jsx
@@ -1,14 +1,48 @@
 import { createContext, useState } from 'react'
 
+// Helper Function::
+const addCartItem = (cartItem, productToAdd) => {
+  // find if cartItem contains product To add
+  // if found, increment quantity
+  // return new array with modified cartItem / new cart item
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => {},
+  cartItem: [],
+  addItemToCart: () => {},
 })
+
+/* 
+Product
+{
+    id,
+    name,
+    price,
+    imageUrl
+}
+
+Cart Item 
+{
+    id,
+    name,
+    price,
+    imageUrl,
+    quantity
+}
+*/
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false)
+  const [cartItems, setCartItems] = useState([])
+
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd))
+  }
+
   const value = { isCartOpen, setIsCartOpen }
-//   console.log(isCartOpen,1111);
+  //   console.log(isCartOpen,1111);
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 }
