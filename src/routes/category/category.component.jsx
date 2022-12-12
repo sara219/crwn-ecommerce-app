1604a171590ee2a55d1d7a55a489bc0c0d13a9a9
sara219 <sara97abuawwad@gmail.com>
2022-12-12 21:49:32 +0200

    change to styled component

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,10 +1,10 @@
-import { useContext, useState, useEffect, Fragment } from 'react'
+import { useContext, useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import ProductCard from '../../components/product-card/product-card.component'
 
 import { CategoriesContext } from '../../context/categories.context'
 
-import './category.styles.scss'
+import { CategoryContainer, CategoryTitle } from './category.styles'
 
 const Category = () => {
   const { category } = useParams()
@@ -16,16 +16,16 @@ const Category = () => {
   }, [category, categoriesMap])
 
   return (
-    <Fragment>
-      <h2 className='category-title'>{category.toUpperCase()}</h2>
+    <>
+      <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
 
-      <div className='category-container'>
+      <CategoryContainer>
         {products &&
           products.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
-      </div>
-    </Fragment>
+      </CategoryContainer>
+    </>
   )
 }
 
