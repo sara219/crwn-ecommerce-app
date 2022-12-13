4c817200c6f6e3f98cb3f85447976961f3b305cb
sara219 <sara97abuawwad@gmail.com>
2022-12-13 17:31:34 +0200

    change cart item component  style to styled component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,16 +1,23 @@
-import './cart-item.styles.scss'
+import {
+  CartItemContainer,
+  CartItemImage,
+  CartItemDetails,
+  CartItemName,
+  CartItemPrice
+} from './cart-item.styles'
+
 const CartItem = ({ cartItem }) => {
   const { name, imageUrl, price, quantity } = cartItem
   return (
-    <div className='cart-item-container'>
-      <img src={imageUrl} alt={`${name}`} />
-      <div className='item-details'>
-        <span className='name'>{name}</span>
-        <span className='price'>
+    <CartItemContainer>
+      <CartItemImage src={imageUrl} alt={`${name}`} />
+      <CartItemDetails>
+        <CartItemName>{name}</CartItemName>
+        <CartItemPrice>
           {quantity} * ${price}
-        </span>
-      </div>
-    </div>
+        </CartItemPrice>
+      </CartItemDetails>
+    </CartItemContainer>
   )
 }
 
