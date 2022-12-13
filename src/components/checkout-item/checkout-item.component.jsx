fb71a2dfca9bea39e84bbab6ae54aefd212831a5
sara219 <sara97abuawwad@gmail.com>
2022-12-13 15:04:19 +0200

    change the checkout item component  style to styled component

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,7 +1,16 @@
 import { useContext } from 'react'
 import { CartContext } from '../../context/cart.context'
 
-import './checkout-item.styles.scss'
+import {
+  CheckoutItemContainer,
+  ImageContainer,
+  RemoveButton,
+  Arrow,
+  ItemName,
+  ItemPrice,
+  ItemQuantity,
+  ItemValueQuantity,
+} from './checkout-item.styles'
 
 const CheckoutItem = ({ cartItem }) => {
   const { clearItemFromCart, addItemToCart, removeItemFromCart } =
@@ -13,25 +22,19 @@ const CheckoutItem = ({ cartItem }) => {
   const removeItemHandler = () => removeItemFromCart(cartItem)
 
   return (
-    <div className='checkout-item-container'>
-      <div className='image-container'>
+    <CheckoutItemContainer>
+      <ImageContainer>
         <img src={imageUrl} alt={`${imageUrl}`} />
-      </div>
-      <span className='name'>{name}</span>
-      <span className='quantity'>
-        <div className='arrow' onClick={addItemHandler}>
-          &#10094;
-        </div>
-        <span className='value'>{quantity}</span>
-        <div className='arrow' onClick={removeItemHandler}>
-          &#10095;
-        </div>
-      </span>
-      <span className='price'>{price}</span>
-      <div className='remove-button' onClick={ClearItemHandler}>
-        &#10005;
-      </div>
-    </div>
+      </ImageContainer>
+      <ItemName>{name}</ItemName>
+      <ItemQuantity>
+        <Arrow onClick={addItemHandler}>&#10094;</Arrow>
+        <ItemValueQuantity>{quantity}</ItemValueQuantity>
+        <Arrow onClick={removeItemHandler}>&#10095;</Arrow>
+      </ItemQuantity>
+      <ItemPrice>{price}</ItemPrice>
+      <RemoveButton onClick={ClearItemHandler}>&#10005;</RemoveButton>
+    </CheckoutItemContainer>
   )
 }
 
---
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -0,0 +1,47 @@
+import styled from 'styled-components'
+
+export const CheckoutItemContainer = styled.div`
+  width: 100%;
+  display: flex;
+  min-height: 100px;
+  border-bottom: 1px solid darkgrey;
+  padding: 15px 0;
+  font-size: 20px;
+  align-items: center;
+`
+
+export const ImageContainer = styled.div`
+  width: 23%;
+  padding-right: 15px;
+
+  img {
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export const ItemName = styled.span`
+  width: 23%;
+`
+
+export const ItemPrice = styled.span`
+  width: 23%;
+`
+
+export const ItemQuantity = styled.span`
+  display: flex;
+  width: 23%;
+`
+
+export const ItemValueQuantity = styled.span`
+  margin: 0 10px;
+`
+
+export const Arrow = styled.div`
+  cursor: pointer;
+`
+
+export const RemoveButton = styled.div`
+  padding-left: 12px;
+  cursor: pointer;
+`
