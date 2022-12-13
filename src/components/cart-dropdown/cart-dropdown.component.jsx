c780d8edb93b3f74a755116c623e124b010802e1
sara219 <sara97abuawwad@gmail.com>
2022-12-13 13:21:44 +0200

    change the cart dropdown style to styled component and add msg when the cart is empty

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -6,7 +6,11 @@ import { CartContext } from '../../context/cart.context'
 import Button from '../button/button.component'
 import CartItem from '../cart-item/cart-item.component'
 
-import './cart-dropdown.styles.scss'
+import {
+  CartDropdownContainer,
+  EmptyMsg,
+  CartItems,
+} from './cart-dropdown.styles'
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext)
@@ -17,14 +21,16 @@ const CartDropdown = () => {
   }
 
   return (
-    <div className='cart-dropdown-container'>
-      <div className='cart-items'>
-        {cartItems.map((item) => (
-          <CartItem key={item.id} cartItem={item} />
-        ))}
-      </div>
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMsg>Your cart is empty! </EmptyMsg>
+        )}
+      </CartItems>
       <Button onClick={goToCheckoutHandler}>CHECKOUT</Button>
-    </div>
+    </CartDropdownContainer>
   )
 }
 
