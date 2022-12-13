8ddba42447545be72f7a02d3690d808889259bfa
sara219 <sara97abuawwad@gmail.com>
2022-12-13 12:49:06 +0200

    change the button component style to styled component

--- src/components/button/button.component.jsx
+++ src/components/button/button.component.jsx
@@ -5,22 +5,24 @@ three diff type of button:
 3. google sing in
 */
 
-import './button.styles.scss'
+import { BaseButton, GoogleSignInButton, InvertedButton } from './button.styles'
 
 const BUTTON_TYPE_CLASSES = {
+  base: 'base',
   google: 'google-sign-in',
   inverted: 'inverted',
 }
 
+const getButton = (buttonType = BUTTON_TYPE_CLASSES.base) =>
+  ({
+    [BUTTON_TYPE_CLASSES.base]: BaseButton,
+    [BUTTON_TYPE_CLASSES.google]: GoogleSignInButton,
+    [BUTTON_TYPE_CLASSES.inverted]: InvertedButton,
+  }[buttonType])
+
 const Button = ({ children, buttonType, ...otherProps }) => {
-  return (
-    <button
-      className={`button-container ${BUTTON_TYPE_CLASSES[buttonType]}`}
-      {...otherProps}
-    >
-      {children}
-    </button>
-  )
+  const CustomBtn = getButton(buttonType)
+  return <CustomBtn {...otherProps}>{children}</CustomBtn>
 }
 
 export default Button
